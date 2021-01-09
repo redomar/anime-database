aea735c672decc9b032c12f2a4ccefe924e256bb
Mohamed Omar <Mohamed12@live.co.uk>
2021-01-09 04:56:47 +0000

    Designing the homepage

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,126 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap"
+    rel="stylesheet"
+  />
+  <div class="app">
+    <header>
+      <h1>The<strong>Anime</strong>Database</h1>
+
+      <form class="search-box">
+        <input
+          type="search"
+          class="search-field"
+          placeholder="Search for an anime..."
+          required
+        />
+      </form>
+    </header>
+    <main>
+      <div class="cards">
+        <Card />
+      </div>
+    </main>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue'
+import Card from './components/Card.vue'
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld
+    Card
   }
-});
+})
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+// text-shadow: 1px 1px 9px #0070f333;
+// color: #0070f3;
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+  font-family: 'Inter', sans-serif;
+}
+
+a,
+a:visited {
+  color: black;
+  text-decoration: none;
+}
+
+header {
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+
+  h1 {
+    font-size: 2.6em;
+    font-weight: 400;
+    text-align: center;
+    text-transform: uppercase;
+    margin-bottom: 30px;
+    // color: #0070f3;
+    // text-shadow: 1px 1px 6px #0070f333;
+  }
+
+  strong {
+    color: #0070f3;
+  }
+
+  &:hover {
+    cursor: default;
+  }
+
+  .search-box {
+    display: flex;
+    justify-content: center;
+    padding-left: 30px;
+    padding-right: 30px;
+
+    .search-field {
+      appearance: none;
+      background: none;
+      border: none;
+      outline: none;
+
+      background-color: rgba($color: #0070f3, $alpha: 0.1);
+      box-shadow: 0px 4px 8px rgba($color: #0070f3, $alpha: 0.15);
+
+      display: block;
+      width: 100%;
+      max-width: 600px;
+      padding: 15px;
+      border-radius: 8px;
+
+      &::placeholder {
+        color: #13437a;
+      }
+
+      &:focus,
+      &:valid {
+        background: #0cf8f9;
+        box-shadow: 0px 0px 0px;
+      }
+    }
+  }
+}
+
+main {
+  max-width: 75rem;
+  margin: 0 auto;
+  padding: 30px;
+  padding-right: 30px;
+
+  .cards {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0 -0.5rem;
+  }
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="card">
+    <a href="#" target="_blank" rel="noopener noreferrer">
+      <img src="https://placehold.it/150x250" alt="Anime Poster" />
+      <h3>Sample</h3>
+    </a>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  flex: 1 1 33;
+  width: 33%;
+  max-width: 33%;
+  padding: 0px 0.5rem;
+  margin-bottom: 1rem;
+
+  img {
+    // width: 100%;
+    height: 300px;
+    object-fit: cover;
+    border-radius: 0.5rem;
+    box-shadow: 0px 4px 8px rgba($color: #0070f3, $alpha: 0.15);
+    transition: 0.4s;
+  }
+
+  h3 {
+    padding: 0.6rem;
+    color: #0070f3;
+    transition: 0.4s;
+  }
+
+  &:hover {
+    img {
+      transform: scale(1.04);
+    }
+    h3 {
+      color: #0b35db;
+    }
+  }
+}
+</style>
